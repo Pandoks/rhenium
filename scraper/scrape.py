b4693b6b5f83451a6d6a59d6b43ae8fe337997de
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-05-04 03:15:10 -0400

    Price history scrapes

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,4 +1,5 @@
 import re
+import time
 from playwright.sync_api import sync_playwright
 
 
@@ -10,6 +11,40 @@ def get(url):
         browser = chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(url)
+
+        page.wait_for_load_state("domcontentloaded")
+        price_history_element = page.locator('h2:text-is("Price history")').locator(
+            "xpath=.."
+        )
+        button = price_history_element.locator('span:text-is("Show more")').locator(
+            "xpath=.."
+        )
+        if button.count() > 0:
+            button.click()
+
+        tax_history_element = page.locator('h2:text-is("Public tax history")').locator(
+            "xpath=.."
+        )
+        button = tax_history_element.locator('span:text-is("Show more")').locator(
+            "xpath=.."
+        )
+        if button.count() > 0:
+            button.click()
+
+        facts_element = page.locator('h4:text-is("Facts and features")').locator(
+            "xpath=.."
+        )
+        if not facts_element.count():
+            facts_element = page.locator('h2:text-is("Facts & features")').locator(
+                "xpath=.."
+            )
+        button = facts_element.locator(
+            'span:text-is("See more facts and features")'
+        ).locator("xpath=..")
+        if not button.count():
+            button = facts_element.locator('span:text-is("Show more")')
+        if button.count() > 0:
+            button.click()
         page.wait_for_load_state("domcontentloaded")
 
         isListing = False
@@ -51,43 +86,61 @@ def get(url):
         print("state:", state)
         print("zip:", zip)
 
-        price_history_table_element = None
-        if isListing:
-            price_history_table_element = page.query_selector(
-                "table.StyledTableComponents__StyledTable-fshdp-8-100-2__sc-shu7eb-2.jaWGxh"
-            )
-        else:
-            price_history_table_element = page.query_selector(
-                "table.StyledTableComponents__StyledTable-sc-f00yqe-2 kNXiqz"
-            )
-        if price_history_table_element:
-            price_history_table_element = price_history_table_element.query_selector(
-                "tbody"
-            )
         price_history = []
-        if price_history_table_element:
-            price_history_rows = price_history_table_element.query_selector_all(
-                "tr[id]"
+        for price_rows in (
+            price_history_element.locator("tbody").locator("tr[id]").element_handles()
+        ):
+            price_columns = price_rows.query_selector_all("td")
+            price_date = price_columns[0].query_selector("span").inner_text()
+            price_event = price_columns[1].query_selector("span").inner_text()
+            price_price = (
+                price_columns[2]
+                .query_selector("span")
+                .query_selector("span")
+                .inner_text()
+                .replace("$", "")
+                .replace(",", "")
             )
-            for price_rows in price_history_rows:
-                price_history_columns = price_rows.query_selector_all("td")
-                price_date = (
-                    price_history_columns[0].query_selector("span").inner_text()
-                )
-                price_event = (
-                    price_history_columns[1].query_selector("span").inner_text()
-                )
-                price_price = (
-                    price_history_columns[2]
-                    .query_selector("span")
-                    .query_selector("span")
-                    .inner_text()
-                    .replace("$", "")
-                    .replace(",", "")
-                )
-                price_history.append([price_date, price_event, price_price])
+            price_history.append([price_date, price_event, price_price])
+        print("price history:", price_history)
 
-        print(price_history)
+        tax_history = []
+        for tax_rows in (
+            tax_history_element.locator("tbody").locator("tr[id]").element_handles()
+        ):
+            tax_columns = tax_rows.query_selector_all("td")
+            tax_year = tax_columns[0].query_selector("span").inner_text()
+            property_taxes = (
+                tax_columns[1]
+                .query_selector("span")
+                .inner_text()
+                .replace("$", "")
+                .replace(",", "")
+                .split(" ")[0]
+            )
+            if property_taxes == "--":
+                property_taxes = None
+            tax_assessment = (
+                tax_columns[2]
+                .query_selector("span")
+                .inner_text()
+                .replace("$", "")
+                .replace(",", "")
+                .split(" ")[0]
+            )
+            tax_history.append([tax_year, property_taxes, tax_assessment])
+        print("tax history:", tax_history)
+
+        building_type = None
+        year_built = None
+        bedrooms = None
+        bathrooms = None
+        lot_size = None
+        print(facts_element.locator('span:has-text("Bedrooms")').inner_text())
+        print(facts_element.locator('span:text-matches("Bathrooms")').inner_text())
+        print(facts_element.locator('span:has-text("Year built")').inner_text())
+        print(facts_element.locator('span:has-text("Home type")').inner_text())
+        print(facts_element.locator('span:has-text("Lot size")').inner_text())
 
         browser.close()
 
@@ -95,3 +148,6 @@ def get(url):
 
 
 get("https://www.zillow.com/homedetails/19615387_zpid/")
+# get(
+#     "https://www.zillow.com/homedetails/875-Cotati-Trl-5-Sunnyvale-CA-94085/300481656_zpid/"
+# )
