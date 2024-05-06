2d3a823e2220a3034e9d69cdefade38834540d2d
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-05-05 23:43:10 -0400

    Added price_history table

--- scraper/schema.sql
+++ scraper/schema.sql
@@ -19,3 +19,15 @@ CREATE TABLE tax_history (
   PRIMARY KEY (address, city, zip, state, year),
   FOREIGN KEY (address, city, zip, state) REFERENCES properties(address, city, zip, state)
 );
+
+CREATE TABLE price_history (
+  date DATE,
+  event TEXT,
+  price MONEY,
+  address TEXT,
+  city TEXT,
+  zip VARCHAR(10),
+  state VARCHAR(2),
+  PRIMARY KEY (address, city, zip, state, date),
+  FOREIGN KEY (address, city, zip, state) REFERENCES properties(address, city, zip, state)
+);
